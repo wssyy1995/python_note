0e0d0a28bcb09c398290ff2f24c15503bf9f76f7
wssyy1995 <18221091524@126.com>
2019-08-06 00:05:15 +0800

    update

--- 函数/匿名函数.py
+++ 函数/匿名函数.py
@@ -14,6 +14,10 @@ calc2 = lambda n:n**n
 #例子 ：求字典里value最大的key
 
 dic={'k1':111,'k2':222,'k3':333}
-print(max(dic,key=lambda k:dic[k]))
+print(sorted(dic,key=lambda k:dic[k]))
+sorted是从小到大排序的
+#相当于将dic进行for i in dic,将i 作为参数传入k ，然后根据dic[k]的值大小进行排序
 
 #匿名函数都可以跟带key的函数合作
+
+
